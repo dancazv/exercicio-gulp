f44e613c78cf53e9a3aa6d082cde156daa73ba78
danzinazevedo <148352990+danzinazevedo@users.noreply.github.com>
2024-08-20 11:12:01 -0300

    commit incial

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+
+function compilaSass(){
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(sass()).pipe(gulp.dest('./build/styles'));
+        
+}
+
+function funcaoPadrao(callback){
+    setTimeout(function(){
+    console.log("Executando via Gulp");
+    callback();
+    }, 2000)
+}
+
+function dizOi(callback){
+    console.log("Olá Gulp");
+        dizTchau();
+        callback();
+}
+
+function dizTchau(callback){
+    console.log("Tchau Gulp");
+}
+
+exports.default = gulp.parallel(funcaoPadrao, dizOi);
+exports.dizOi = dizOi;
+exports.sass = compilaSass;
