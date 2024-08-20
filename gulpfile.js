547912cfb694c0584bd709d99b17377dacf09e21
danzinazevedo <148352990+danzinazevedo@users.noreply.github.com>
2024-08-20 12:16:39 -0300

    atual

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,50 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
 
-function compilaSass(){
-    return gulp.src('./source/styles/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(sass()).pipe(gulp.dest('./build/styles'));
-        
-}
+compilaSass = () => {
+	return gulp
+		.src("./source/styles/main.scss")
+		.pipe(sourcemaps.init())
+		.pipe(
+			sass({
+				outputStyle: "compressed",
+			}),
+		)
+		.pipe(sourcemaps.write("./maps"))
+		.pipe(gulp.dest("./build/styles"));
+};
 
-function funcaoPadrao(callback){
-    setTimeout(function(){
-    console.log("Executando via Gulp");
-    callback();
-    }, 2000)
-}
+comprimeImagens = () => {
+	return gulp
+		.src("./source/img/*")
+		.pipe(imagemin())
+		.pipe(gulp.dest("./build/img"));
+};
 
-function dizOi(callback){
-    console.log("Olá Gulp");
-        dizTchau();
-        callback();
-}
+comprimeJavaScript = () => {
+	return gulp
+		.src("./source/scripts/*.js")
+		.pipe(uglify())
+		.pipe(gulp.dest("./build/scripts"));
+};
 
-function dizTchau(callback){
-    console.log("Tchau Gulp");
-}
-
-exports.default = gulp.parallel(funcaoPadrao, dizOi);
-exports.dizOi = dizOi;
-exports.sass = compilaSass;
+exports.default = () => {
+	gulp.watch(
+		"./source/styles/*.scss",
+		{ ignoreInitial: false },
+		gulp.series(compilaSass),
+	);
+	gulp.watch(
+		"./source/img/*",
+		{ ignoreInitial: false },
+		gulp.series(comprimeImagens),
+	);
+	gulp.watch(
+		"./source/scripts/*.js",
+		{ ignoreInitial: false },
+		gulp.series(comprimeJavaScript),
+	);
+};
